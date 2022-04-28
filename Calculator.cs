a3e425efcfe97f410ff38810413ebf2a89a34e39
Mikkel F <mikkelinis@gmail.com>
2022-04-28 12:28:17 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Speed_Conveter;
+
+namespace SpeedConveter
+{
+    public class Calculator
+    {
+        public static string Prefix(char input1, char input2)
+        {
+            string prefix1 = "", prefix2 = "";
+
+            switch (input1)
+            {
+                case '1':
+                    prefix1 = "m/s";
+                    break;
+                case '2':
+                    prefix1 = "km/h";
+                    break;
+                case '3':
+                    prefix1 = "mph";
+                    break;
+                case '4':
+                    prefix1 = "knots";
+                    break;
+                default:
+                    prefix1 = "Prefix Error 1";
+                    break;
+            }
+
+            switch (input2)
+            {
+                case '1':
+                    prefix2 = "m/s";
+                    break;
+                case '2':
+                    prefix2 = "km/h";
+                    break;
+                case '3':
+                    prefix2 = "mph";
+                    break;
+                case '4':
+                    prefix2 = "knots";
+                    break;
+                default:
+                    prefix2 = "Prefix Error 2";
+                    break;
+            }
+           return ($"{prefix1}-{prefix2}");
+        }
+
+        public static double SpeedCalculator1(double speed1, double speed2, char input1, char input2)
+        {
+
+            string prefix = Calculator.Prefix(input1, input2);
+            string[] split = prefix.Split('-');
+            string prefix1 = split[0], prefix2 = split[1];
+
+            string errmsg = $"You can't convert {prefix1} to {prefix2}";
+
+            //what prefix is speed1
+            switch (input1)
+            {
+                //meters pr. sec. m/s.
+                case '1':
+                    //what prefix is speed2.
+                    switch (input2)
+                    {
+                        //m/s -> km/h.
+                        case '2':
+                            speed2 = speed1 * 3.6;
+                            break;
+
+                        //m/s -> mph.
+                        case '3':
+                            speed2 = speed1 / 0.4470;
+                            break;
+
+                        //m/s -> knots.
+                        case '4':
+                            speed2 = speed1 * 1.9438;
+                            break;
+                    }
+                    break;
+
+                //kilometers pr. hour km/h.
+                case '2':
+                    //what prefix is speed2.
+                    switch (input2)
+                    {
+                        //km/h -> m/s.
+                        case '1':
+                            speed2 = speed1 / 3.6;
+                            break;
+
+                        //km/h -> mph.
+                        case '2':
+                            speed2 = speed1 * 0.6214;
+                            break;
+
+                        //km/h -> knots.
+                        case '4':
+                            speed2 = speed1 * 1.852;
+                            break;
+                    }
+                    break;
+
+                //miles pr. hour mph.
+                case '3':
+                    //what prefix is speed2.
+                    switch (input2)
+                    {
+                        //mph -> m/s.
+                        case '1':
+                            speed2 = speed1 * 0.4470;
+                            break;
+
+                        //mph -> km/h.
+                        case '2':
+                            speed2 = speed1 * 1.6093;
+                            break;
+
+                        //mph -> knots.
+                        case '4':
+                            speed2 = speed1 / 1.1508;
+                            break;
+                    }
+                    break;
+
+                //knots.
+                case '4':
+                    //what prefix is speed2.
+                    switch (input2)
+                    {
+                        //knots -> m/s.
+                        case '1':
+                            speed2 = speed1 * 0.5144;
+                            break;
+
+                        //knots -> km/h.
+                        case '2':
+                            speed2 = speed1 * 0.54;
+                            break;
+
+                        //knots -> mph.
+                        case '3':
+                            speed2 = speed1 * 1.1508;
+                            break;
+                    }
+                    break;
+                //Error message.
+                default:
+                    Console.Write(errmsg);
+                    break;
+            }
+            return speed2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using SpeedConveter;
+
+namespace Speed_Conveter
+{
+    public class WsCon
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            char input1 = ' ', input2 = ' ';
+            string tofrom = "";
+            double speed1 = 0, speed2 = 0;
+            bool parse;
+
+            for (i = 0; i < 1; i++)
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n    Windspeede Converter" +
+                                    "\n    It is not possible to convert from and to the same prefix.");
+                    if (i == 0)
+                    {
+                        tofrom = "from";
+                    }
+                    else if (i == 1)
+                    {
+                        tofrom = "to";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+                    }
+
+                    Console.WriteLine($"\n    Which of the following do you wish to convert {tofrom}");
+                    Console.WriteLine("\n    m/s    [ 1 ]" +
+                                      "\n    Km/t   [ 2 ]" +
+                                      "\n    mph    [ 3 ]" +
+                                      "\n    knots  [ 4 ]");
+
+                    Console.SetCursorPosition(4, 10);
+                    input1 = Console.ReadKey().KeyChar;
+                    Console.SetCursorPosition(4, 10);
+                    Console.WriteLine(" ");
+
+                    Console.SetCursorPosition(4, 11);
+                    Console.WriteLine($"Please enter the speed value");
+                    Console.SetCursorPosition(4, 12);
+                    parse = double.TryParse(Console.ReadLine(), out speed1);
+
+                    if (!parse)
+                    {
+                        Console.Clear();
+                        Console.SetCursorPosition(5, 5);
+                        Console.WriteLine("Error. No value set.");
+                        Thread.Sleep(1500);
+
+                    }
+
+                } while (!parse);
+
+
+                Console.SetCursorPosition(4, 11);
+                Console.WriteLine("                            \n                             ");
+                Console.SetCursorPosition(4, 10);
+                input2 = Console.ReadKey().KeyChar;
+                Console.SetCursorPosition(4, 10);
+                Console.WriteLine(" ");
+                Console.Clear();
+            }
+
+            string prefix = Calculator.Prefix(input1, input2);
+            string[] split = prefix.Split('-');
+            string prefix1 = split[0], prefix2 = split[1];
+
+            speed2 = Calculator.SpeedCalculator1(speed1, speed2, input1, input2);
+
+            Console.WriteLine($"{speed1} {prefix1} = {speed2} {prefix2}");
+        }
+    }
+}
